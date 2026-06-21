2389ac449adfffaa2dfa3e97339a356432405d71
dieisson <dieisson.qfloriano@gmail.com>
2026-06-20 22:32:25 -0300

    Criado Excepiton customizada

---
+++ Avaliacao-m8-Alvaro-Dieisson-Kaio-Matheus/Execoes/LivroNaoEncontradoExepition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avaliacao_m8_Alvaro_Dieisson_Kaio_Matheus.Execoes
+{
+    public class LivroNaoEncontradoExepition : Exception
+    {
+        public LivroNaoEncontradoExepition(string mensagem) : base(mensagem)
+        {
+        }
+    }
+}
