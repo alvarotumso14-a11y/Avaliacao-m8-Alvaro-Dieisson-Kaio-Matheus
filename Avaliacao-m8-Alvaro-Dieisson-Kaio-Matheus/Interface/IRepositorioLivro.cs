1c2a1496fef25f456c5730265589ae3befb26c67
Álvaro Santos Ribeiro <Alvarotumso14@gmail.com>
2026-06-20 22:37:37 -0300

    Criacao Interface

---
+++ Avaliacao-m8-Alvaro-Dieisson-Kaio-Matheus/Interface/IRepositorioLivro.cs
@@ -0,0 +1,18 @@
+﻿using ProjetoBiblioteca.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoBiblioteca.Interface
+{
+    public interface IRepositorioLivro
+    {
+        void Adicionar(Livro livro);
+        List<Livro> ListarTodos();
+        Livro BuscarPorId(int id);
+
+        List<Livro> BuscarPorAutor(string autor);
+    }
+}
